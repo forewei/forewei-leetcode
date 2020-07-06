3cabb785e44b3008a6038cfe09fe4ad42cf8bd3e
forewei <597082933@qq.com>
2020-07-06 11:23:51 +0800

    Add chapter6

---
+++ src/main/java/array/Chapter6.java
@@ -0,0 +1,111 @@
+package array;
+
+import org.junit.Test;
+
+/**
+ * 存在重复元素     https://leetcode-cn.com/explore/featured/card/top-interview-questions-easy/1/array/24/
+ * <p>
+ * <p>
+ * 给定一个整数数组，判断是否存在重复元素。
+ * <p>
+ * 如果任意一值在数组中出现至少两次，函数返回 true 。如果数组中每个元素都不相同，则返回 false 。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [1,2,3,1]
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入: [1,2,3,4]
+ * 输出: false
+ * 示例 3:
+ * <p>
+ * 输入: [1,1,1,3,3,4,3,2,4,2]
+ * 输出: true
+ *
+ * @auther forewei
+ * @date 2020/7/6
+ */
+public class Chapter6 {
+
+    public boolean containsDuplicate(int[] nums) {
+        if (nums.length <= 1) {
+            return false;
+        }
+        heapSort(nums);
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 创建堆，
+     *
+     * @param arr 待排序列
+     */
+    private static void heapSort(int[] arr) {
+        //创建堆
+        for (int i = (arr.length - 1) / 2; i >= 0; i--) {
+            //从第一个非叶子结点从下至上，从右至左调整结构
+            adjustHeap(arr, i, arr.length);
+        }
+
+        //调整堆结构+交换堆顶元素与末尾元素
+        for (int i = arr.length - 1; i > 0; i--) {
+            //将堆顶元素与末尾元素进行交换
+            int temp = arr[i];
+            arr[i] = arr[0];
+            arr[0] = temp;
+
+            //重新对堆进行调整
+            adjustHeap(arr, 0, i);
+        }
+    }
+
+    /**
+     * 调整堆
+     *
+     * @param arr    待排序列
+     * @param parent 父节点
+     * @param length 待排序列尾元素索引
+     */
+    private static void adjustHeap(int[] arr, int parent, int length) {
+        //将temp作为父节点
+        int temp = arr[parent];
+        //左孩子
+        int lChild = 2 * parent + 1;
+
+        while (lChild < length) {
+            //右孩子
+            int rChild = lChild + 1;
+            // 如果有右孩子结点，并且右孩子结点的值大于左孩子结点，则选取右孩子结点
+            if (rChild < length && arr[lChild] < arr[rChild]) {
+                lChild++;
+            }
+
+            // 如果父结点的值已经大于孩子结点的值，则直接结束
+            if (temp >= arr[lChild]) {
+                break;
+            }
+
+            // 把孩子结点的值赋给父结点
+            arr[parent] = arr[lChild];
+
+            //选取孩子结点的左孩子结点,继续向下筛选
+            parent = lChild;
+            lChild = 2 * lChild + 1;
+        }
+        arr[parent] = temp;
+    }
+
+    @Test
+    public void test() {
+        int[] nums = {1, 2, 10, 3, 4, 5, 6};
+        System.out.println(this.containsDuplicate(nums));
+    }
+}
