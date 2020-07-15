bd94a4a834a1c16718797f9927feb97cdde52384
forewei <forewei2015@gmail.com>
2020-07-15 17:46:42 +0800

    Add Chapter13

---
+++ src/main/java/array/Chapter13.java
@@ -0,0 +1,41 @@
+package array;
+
+import org.junit.Test;
+
+/**
+ * 区域和检索 - 数组不可变 https://leetcode-cn.com/problems/range-sum-query-immutable/
+ * <p>
+ * 给定一个整数数组  nums，求出数组从索引 i 到 j  (i ≤ j) 范围内元素的总和，包含 i,  j 两点。
+ * <p>
+ * 示例：
+ * <p>
+ * 给定 nums = [-2, 0, 3, -5, 2, -1]，求和函数为 sumRange()
+ * <p>
+ * sumRange(0, 2) -> 1
+ * sumRange(2, 5) -> -1
+ * sumRange(0, 5) -> -3
+ * 说明:
+ * <p>
+ * 你可以假设数组不可变。
+ * 会多次调用 sumRange 方法。
+ *
+ * @author: forewei
+ * @date: 2020-07-15
+ **/
+public class Chapter13 {
+    private int[] data;
+
+
+    public Chapter13(int[] nums) {
+        data = nums;
+    }
+
+    public int sumRange(int i, int j) {
+        int sum = 0;
+        for (int k = i; k <= j; k++) {
+            sum += data[k];
+        }
+        return sum;
+    }
+}
+
